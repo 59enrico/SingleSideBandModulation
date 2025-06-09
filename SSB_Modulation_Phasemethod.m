ca4adea5303d7e0b7c3bc563b73e28bafdc01a11
eleue <eleue@stud.hs-offenburg.de>
2025-06-09 22:16:52 +0200

    refined and expanded description, to better fit the requirements of the project; refined line comments to document core signal processing elements; added variable check for speaker playback duration, fixed to signal length if custom variable exceeds signals length;

--- SSB_Modulation_Phasemethod.m
+++ SSB_Modulation_Phasemethod.m
@@ -1,40 +1,40 @@
-%% Leue, Enrico - MT/189104 - 05.06.2025
+%% Leue, Enrico - MT/189104 - 09.06.2025
 % --------------------------------------
-% University of Applied Sciences Offenburg - Digital Signal Processing SS2025 - Project: Single Sideband (SSB) modulation
+% University of Applied Sciences Offenburg - Digital Signal Processing SS2025 - Single Sideband (SSB) modulation
 % --------------------------------------
-% This project implements single sideband (SSB) modulation of an audio signal using the Hilbert transform and the phase method.
-% The goal is to shift a given audio signal in frequency so that only the upper sideband remains.
-% The script loads an audio signal, computes its analytic signal using the Hilbert transform, and modulates it with a carrier signal.
-% The spectra of both the original and the SSB-modulated signals are then calculated and plotted to visualize the frequency shift.
-% To demonstrate the effect of modulation, both signals can also be played back via speakers for direct comparison.
+% This project implements the upper single sideband (SSB) modulation of an audio signal using the Hilbert transform and the phase method.
+% The goal is to translate the frequency content of a given audio signal upwards by a variable amount in a generally real-time capable approach.
+% The script loads an audio signal, computes its analytic signal using the Hilbert transform, and modulates it with a complex carrier signal.
+% The spectra of both signals (original and modulated) are calculated and plotted to visualize the frequency shift.
+% To demonstrate the effect of the modulation, both signals are also played back sequentially via speakers for comparison.
 
-clear, close, clc
+clear, close, clc                           % Clear workspace, close figures, clear command window.
 
 %% Input audio signal
-% A) use matlab example file:
-load handel.mat;                            % examples: chirp.mat (1.6 s), gong.mat (5.1 s), handel.mat (8.9 s), laughter.mat (6.4 s)
-% B) use own file:
-% [y, Fs] = audioread('...');
-N = length(y);                              % [#] number of samples in the original audiofile
-t = (0:N-1)/Fs;                             % [s] time vector of the original samples
+% A) using MATLAB example file:
+load chirp.mat;                            % MATLAB examples: chirp.mat (1.6 s), gong.mat (5.1 s), handel.mat (8.9 s), laughter.mat (6.4 s).
+% B) using own file:
+% [y, Fs] = audioread('C:\myFolder\myFile.sample_audio.wav');
+N = length(y);                              % Number of samples in the original file.
+t = (0:N-1)/Fs;                             % [s] time vector corresponding to the original samples.
 
 %% Modulation and carrier
-Fc = 200;                                   % [Hz] frequency by which the signal is modulated up
-modulator = exp(1j*2*pi*Fc*t');             % complex sine function (carrier)
+Fc = 200;                                   % [Hz] Carrier frequency for modulation, equals frequency shift.
+modulator = exp(1j*2*pi*Fc*t');             % Generate the complex exponential function (carrier) used for frequency translation of the input signal.
 
 %% Windowing
-y = y .* hann(N);                           % reduce spectral leakage and prevent discontinuities (i.e. in block bounderies of real time audio)
+y = y .* hann(N);                           % Windowing the signal reduces spectral leakage and prevents discontinuities (i.e. in bounderies of buffered real-time data).
 
 %% Hilbert transform
-x_hilbert = hilbert(y);                     % analytic signal form to isolate positive frequencies of signal
+y_hilbert = hilbert(y);                     % Compute the analytic signal form to isolate positive frequency components of signal, required for SSB modulation.
 
 %% Single Sideband modulation
-ssb_signal = real(x_hilbert .* modulator);  % modulation by multiplication of signal with carrier
+ssb_signal = real(y_hilbert .* modulator);  % Upper SSB modulated signal is real part of the multiplication of the analytic signal with the complex carrier.
 
 %% Calculate spectra
-Y_fft = abs(fft(y, N));                     % fast fourier transform to get spectra of original signal ...
-SSB_fft = abs(fft(ssb_signal, N));          % ... and SSB modulated signal
-f = linspace(0, Fs/2, floor(N/2)+1);        % frequency vector from 0 to Nyquist-frequency of signal
+Y_fft = abs(fft(y, N));                     % Compute magnitude spectra of the original ...
+SSB_fft = abs(fft(ssb_signal, N));          % ... and the SSB modulated signal for visualization.
+f = linspace(0, Fs/2, floor(N/2)+1);        % Corresponding frequency vector up to Nyquist frequency.
 
 %% Plot spectra and visualize shift
 figure;
@@ -45,7 +45,7 @@ plot(f/1e3, SSB_fft(1:floor(N/2)+1), "LineWidth", 2, "DisplayName", 'SSB signal'
 title("Spectra of the signals, shifted up by: "+ num2str(Fc)+ " Hz.");
 xlabel('Frequency [kHz]');
 ylabel('Amplitude');
-xlim([0 1]);                                % focus on the frequency shift in the lower frequencies
+xlim([0 1]);                                % Limited X-Axis to emphasize the frequency shift
 legend();
 
 subplot(2,1,2);
@@ -55,14 +55,15 @@ plot(f/1e3, 20*log10(SSB_fft(1:floor(N/2)+1)), "LineWidth", 2, "DisplayName", 'S
 title("Spectra of the signals in dB, shifted up by: "+ num2str(Fc)+ " Hz.");
 xlabel('Frequency [kHz]');
 ylabel('Amplitude [dB]');
-xlim([0 1]);                                % focus on the frequency shift in the lower frequencies
+xlim([0 1]);                                % Limited X-Axis to emphasize the frequency shift
 legend();
 
 
 %% Speaker playback
-t0 = 0;                                     % [s] start of playback (i.e. 37 s)
-dur = 5;                                    % [s] duration of playback (i.e. 8 s)
-playtime = t0*Fs+1:(t0+dur)*Fs;             % calculate the samples to be played back
-soundsc(y(playtime), Fs);                   % playback the original signal via speaker
-pause(dur+1);                               % pause between playbacks
-soundsc(ssb_signal(playtime), Fs);          % playback the ssb signal via speaker
+t0 = 0;                                     % [s] Start of playback (changeable for custom files, for examples use 0).
+dur = 5;                                    % [s] Duration of playback.
+dur = min(dur, N/Fs);                       % Set maximum playback duration to signal length.
+playtime = t0*Fs+1:(t0+dur)*Fs;             % Calculate the samples to be played back.
+soundsc(y(playtime), Fs);                   % Playback the original signal.
+pause(dur+1);                               % Pause between playbacks.
+soundsc(ssb_signal(playtime), Fs);          % Playback the ssb signal.
